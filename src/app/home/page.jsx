b4cbd8488805096f449d1708db4da670edb27b48
Justin Nguyen <Justin.Nguyen77@yahoo.com>
2025-07-27 10:08:36 -0500

    setting up project structure to begin working on frontend pages

---
+++ src/app/home/page.jsx
@@ -0,0 +1,15 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="">College Exchange</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Home;
